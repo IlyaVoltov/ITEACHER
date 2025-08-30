8cf2caa2bc2904af2131def27e967add6487124b
V Ilia from Linux <ivolovp@gmail.com>
2025-08-30 16:45:02 +0300

    add new file

---
+++ lesson_about_git.py
@@ -0,0 +1,9 @@
+first_value = 100
+second_value = 200
+third_value = 300
+
+def calculate_sum(a, b, c):
+    return a + b + c
+result = calculate_sum(first_value, second_value, third_value)
+print("The sum is:", result)
+
